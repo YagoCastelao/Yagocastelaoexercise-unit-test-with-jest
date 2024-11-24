cbe45bfe51891b2d5a1ede6a380a43037098cb05
Yago Cima Castelao <yago_castelau@hotmail.com>
2024-11-24 23:55:10 +0000

    hecho

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+// This is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3))
+
+// Export the function to be used on other files 
+// (similar to the keyword "export" when using webpack)
+
 let oneEuroIs = {
     "JPY": 156.5, // Japan yen
     "USD": 1.07, // US dollar
     "GBP": 0.87, // British pound
 };
 
-function fromDollarToYen(dollars) {
-    return (dollars / oneEuroIs["USD"]) * oneEuroIs["JPY"];
-}
+const fromDollarToYen = dollars => (dollars / oneEuroIs["USD"]) * oneEuroIs["JPY"];
 
-function fromEuroToDollar(euros) {
-    return euros * oneEuroIs["USD"];
-}
+const fromEuroToDollar = euros => euros * oneEuroIs["USD"];
 
-function fromYenToPound(yen) {
-    return (yen / oneEuroIs["JPY"]) * oneEuroIs["GBP"];
-}
+const fromYenToPound = yen => (yen / oneEuroIs["JPY"]) * oneEuroIs["GBP"];
 
-module.exports = {fromEuroToDollar, fromDollarToYen, fromYenToPound};
+module.exports = {sum, fromEuroToDollar, fromDollarToYen, fromYenToPound};
--- test.js
+++ test.js
@@ -1,17 +1,29 @@
+// Import the function sum from the app.js file
+const { sum } = require('./app.js');
+
+// Start your first test
+test('adds 14 + 9 to equal 23', () => {
+    // Inside the test we call our sum function with 2 numbers
+    let total = sum(14, 9);
+
+    // We expect the sum of those 2 numbers to be 23
+    expect(total).toBe(23);
+});
+
 test("One euro should be 1.07 dollars", function() {
     // Import the function from app.js
-    const { fromEuroToDollar } = require('./app.js');
+    const {fromEuroToDollar} = require('./app.js');
     expect(fromEuroToDollar(3.5)).toBe(3.745);
-})
+});
 
 test("Conversion from USD to JPY (1 USD = 149.03 JPY)", function() {
     // Import the function from app.js
-    const { fromDollarToYen } = require('./app.js')
-    expect(fromDollarToYen(10)).toBe(1490.3);
-})
+    const {fromDollarToYen} = require('./app.js')
+    expect(fromDollarToYen(2)).toBe(292.5233644859813);
+});
 
 test("Conversion from JPY to GBP (1 JPY = 0.0072 GBP)", function() {
     // Import the function from app.js
-    const { fromYenToPound } = require('./app.js')
-    expect(fromYenToPound(1000)).toBe(7.2);
-})
+    const {fromYenToPound} = require('./app.js')
+    expect(fromYenToPound(100)).toBe(0.5559105431309904);
+});
